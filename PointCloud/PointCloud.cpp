912b555fb00e110c833b6491dee84ce62d07758d
13Mzawa2 <nishizawa-masahiro-ys@ynu.jp>
2015-05-14 20:51:47 +0900

    Revert "コメント変更"
    This reverts commit 7a2fade750c80088ff095e3f0f277887c8b0f7dd.
    
    Conflicts:
    	PointCloud/PointCloud.cpp

--- PointCloud/PointCloud.cpp
+++ PointCloud/PointCloud.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "KinectV1Adapter.h"
 
 int main(void)
@@ -57,4 +58,53 @@ int main(void)
 	cv::destroyAllWindows();
 
 	return 0;
+=======
+#include "KinectV1Adapter.h"
+
+int main(void)
+{
+	//	Kinect v1の簡易使用クラス
+	KinectV1 kSensor(
+		NUI_INITIALIZE_FLAG_USES_COLOR |
+		NUI_INITIALIZE_FLAG_USES_DEPTH_AND_PLAYER_INDEX);
+
+	/// Create a window
+	viz::Viz3d myWindow("Coordinate Frame");
+
+	while (1)
+	{
+		// フレームの更新待ち
+		kSensor.waitFrames();
+
+		// Colorカメラからフレームを取得
+		cv::Mat colorMat;
+		kSensor.getColorFrame(colorMat);
+		// Depthカメラからフレームを取得
+		cv::Mat depthMat, playerMask, grayMat, shadeMat;
+		kSensor.getDepthFrameCoordinated(depthMat);
+		kSensor.cvtDepth2Gray(depthMat, grayMat);
+		//kSensor.depthShading(depthMat, grayMat, shadeMat);	//	実装上の問題でcvtDepth2Gray()は3ch画像
+		// 表示
+		cv::imshow("Color", colorMat);
+		cv::imshow("Depth", grayMat);
+
+		Mat cloudMat, colors;
+		kSensor.cvtDepth2Cloud(depthMat,cloudMat);
+		viz::WCloud cw(cloudMat, colorMat);
+		myWindow.showWidget("Kinect", cw, Affine3d().translate(Vec3d(0.0, 0.0, 0.0)));
+
+		myWindow.spinOnce(10, true);
+
+		// フレームの解放
+		kSensor.releaseFrames();
+
+		if(myWindow.wasStopped())	//	E key
+			break;
+	}
+	//waitKey(1000);
+	kSensor.shutdown();
+	cv::destroyAllWindows();
+
+	return 0;
+>>>>>>> parent of 7a2fade... 繧ｳ繝｡繝ｳ繝亥､画峩
 }
