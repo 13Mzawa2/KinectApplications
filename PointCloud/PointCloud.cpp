d9846b9dc27e6fa0a3c9505911a4c8f0b3f57ee7
13Mzawa2 <nishizawa-masahiro-ys@ynu.jp>
2015-05-14 20:44:18 +0900

    コンフリクト解決用　３

--- PointCloud/PointCloud.cpp
+++ PointCloud/PointCloud.cpp
@@ -7,17 +7,11 @@ int main(void)
 		NUI_INITIALIZE_FLAG_USES_COLOR |
 		NUI_INITIALIZE_FLAG_USES_DEPTH_AND_PLAYER_INDEX);
 
-<<<<<<< HEAD
-	/// Create a window
-	viz::Viz3d myWindow("Coordinate Frame");
-
-=======
 	//auto extractor = cv::DescriptorExtractor::create("ORB");
 
 		//	特徴抽出器オブジェクトの生成
 		Ptr<FeatureDetector> detector = cv::FeatureDetector::create("ORB");
 		std::vector<cv::KeyPoint> keypoint;			//	特徴点の保管場所
->>>>>>> origin/modifingKVA
 	while (1)
 	{
 		// フレームの更新待ち
@@ -28,21 +22,6 @@ int main(void)
 		kSensor.getColorFrame(colorMat);
 		// Depthカメラからフレームを取得
 		cv::Mat depthMat, playerMask, grayMat, shadeMat;
-<<<<<<< HEAD
-		kSensor.getDepthFrameCoordinated(depthMat);
-		kSensor.cvtDepth2Gray(depthMat, grayMat);
-		//kSensor.depthShading(depthMat, grayMat, shadeMat);	//	実装上の問題でcvtDepth2Gray()は3ch画像
-		// 表示
-		cv::imshow("Color", colorMat);
-		cv::imshow("Depth", grayMat);
-
-		Mat cloudMat, colors;
-		kSensor.cvtDepth2Cloud(depthMat,cloudMat);
-		viz::WCloud cw(cloudMat, colorMat);
-		myWindow.showWidget("Kinect", cw, Affine3d().translate(Vec3d(0.0, 0.0, 0.0)));
-
-		myWindow.spinOnce(10, true);
-=======
 		kSensor.getDepthFrame(depthMat);
 		kSensor.cvtDepth2Gray(depthMat, grayMat);
 		kSensor.depthShading(depthMat, grayMat, shadeMat);	//	実装上の問題でcvtDepth2Gray()は3ch画像
@@ -58,17 +37,10 @@ int main(void)
 		//cv::drawKeypoints(featureImg, keypoint, featureImg, Scalar(0, 0, 255));
 
 		//cv::imshow("Feature", featureImg);
->>>>>>> origin/modifingKVA
 
 		// フレームの解放
 		kSensor.releaseFrames();
 
-<<<<<<< HEAD
-		if(myWindow.wasStopped())	//	Press E key　VizでQキーを押して中断されたらTerminateApp()を無視して続行するとよい　中止しようとするとフリーズする
-			break;
-	}
-	//waitKey(1000);
-=======
 <<<<<<< ours
 		// ループの終了判定(Escキー)
 		int key = cv::waitKey(1);
@@ -78,10 +50,9 @@ int main(void)
 		if(myWindow.wasStopped())	//	E key
 >>>>>>> theirs
 			break;
-		}
+	}
 
 	}
->>>>>>> origin/modifingKVA
 	kSensor.shutdown();
 	cv::destroyAllWindows();
 
